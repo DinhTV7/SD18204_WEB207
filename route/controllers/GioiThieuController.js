1e2225536743514e78926ffb5437088251e292b2
Ta Van Dinh <dinhtv7@fpt.edu.vn>
2023-10-03 13:47:43 +0700

    update

--- route/controllers/GioiThieuController.js
+++ route/controllers/GioiThieuController.js
@@ -3,4 +3,29 @@ window.GioiThieuController = function ($scope, $routeParams) {
     console.log($routeParams);
     let id = $routeParams.id;
     console.log(id);
+
+    // Khai báo mảng
+    $scope.students = [
+        {
+            ten: "Tạ Văn Định",
+            namsinh: 2005,
+            chuyennganh: "CNTT"
+        },
+        {
+            ten: "DinhTV7",
+            namsinh: 2005,
+            chuyennganh: "CNTT"
+        }
+    ];
+
+    $scope.createStudent = function () {
+        let newStudent = {
+            ten: $scope.student.name,
+            namsinh: $scope.student.year,
+            chuyennganh: $scope.student.couse
+        }
+    /////////////////////////////////////
+        console.log(newStudent);
+        $scope.students.push(newStudent);
+    }
 }
